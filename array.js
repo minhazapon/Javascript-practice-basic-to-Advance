032b58e1f571eaf1f70858ed4cb1da5548d4c755
minhazapon <151423201+minhazapon@users.noreply.github.com>
2023-12-10 20:03:07 -0800

    Add files via upload

---
+++ array.js
@@ -0,0 +1,19 @@
+
+
+
+var names = new Array(10);
+
+names[0] = "apon";
+names[1] = "mursalin";
+names[2] = "rakib";
+names[3] = "sazal";
+names[4] = "musfiq";
+names[5] = "nishan";
+names[6] = "shazan";
+names[7] = "kabid";
+names[8] = "imran";
+names[9] = "parvez";
+console.log(names);
+
+
+
---
+++ loop an Array.js
@@ -0,0 +1,12 @@
+
+
+var num = [10, 20, 60, 70, 90];
+
+
+for ( var i =0; i<=5; i++)
+{
+
+    console.log(num[i]);
+}
+
+
